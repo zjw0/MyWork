5c5148d509e972ec6ed3210baffe6cf816340aae
jack_zhao <15038707891@163.com>
2019-11-05 15:34:36 +0800

    图片多选框架

--- app/src/main/java/cn/appoa/mywork/MainActivity.java
+++ app/src/main/java/cn/appoa/mywork/MainActivity.java
@@ -2,9 +2,6 @@ package cn.appoa.mywork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SnapHelper;
-
-import com.scwang.smartrefresh.layout.utils.SnackbarUtils;
 
 import cn.appoa.aframework.utils.AtyUtils;
 
@@ -15,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AtyUtils.showShort(this,"集成成功",true);
+
     }
 }
