e15cd5cefe847fafd89841882c42107615e7c609
zhao <1104156701@qq.com>
2020-07-06 16:46:42 +0800

    修改签名地址

--- app/src/main/java/cn/appoa/mywork/MainActivity.java
+++ app/src/main/java/cn/appoa/mywork/MainActivity.java
@@ -1,15 +1,10 @@
 package cn.appoa.mywork;
 
-import android.*;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
-
 
 import butterknife.BindView;
 import cn.appoa.aframework.bean.AppVersion;
-import cn.appoa.aframework.utils.AtyUtils;
 import cn.appoa.mywork.base.BaseActivity;
 import cn.appoa.mywork.presenter.MainPresenter;
 import cn.appoa.mywork.view.MainView;
